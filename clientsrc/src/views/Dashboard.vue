a0f3dbe6a4cc700349a49fa6cd67eec0db4f5609
chris_MSI1 <chris.h.felt@gmail.com>
2020-06-18 11:27:14 -0600

    calendar selection event creation

--- clientsrc/src/views/Dashboard.vue
+++ clientsrc/src/views/Dashboard.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="dashboard container-fluid">
-
     <div class="row">
       <!-- <timeline /> -->
-      <div class="col-9 max-height">
+      <div class="col-8 max-height">
         <Fullcalendar
           defaultView="timeGridDay"
           :plugins="calendarPlugins"
+          :contentHeight="500"
+          :selectable="true"
+          @select="handleSelect"
+          :events="events"
+          :editable="true"
           :header="{
             left: 'title',
             center: 'dayGridMonth, timeGridWeek, timeGridDay, listWeek',
@@ -14,27 +18,28 @@
           }"
         />
       </div>
-      <div class="col-3 ">
+      <div class="col-4 ">
         <div class="row">
-          <assignment />
+          <!-- <assignment /> -->
+          <student
+            v-for="student in students"
+            :key="student.id"
+            :student="student"
+          />
         </div>
       </div>
-
     </div>
-    <div class="row justify-content-center mt-4">
+    <!-- <div class="row justify-content-center mt-4">
       <div class="col-12 shadow">
-        <div class="row justify-content-center students-box bg-info">
-          <student v-for="student in students" :key="student.id" :student="student" />
-        </div>
+        <div class="row justify-content-center students-box bg-info"></div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 import timeline from "../components/TimelineComponent";
 import assignment from "../components/AssignmentComponent";
-
 
 require("@fullcalendar/core/main.min.css");
 require("@fullcalendar/daygrid/main.min.css");
@@ -45,7 +50,6 @@ import DayGridPlugin from "@fullcalendar/daygrid";
 import TimeGridPlugin from "@fullcalendar/timegrid";
 import InteractionPlugin from "@fullcalendar/interaction";
 import ListPlugin from "@fullcalendar/list";
-
 
 import student from "../components/StudentMock";
 
@@ -67,31 +71,44 @@ export default {
   computed: {
     students() {
       return this.$store.state.StudentStore.students;
-    }
+    },
+    events() {
+      return this.$store.state.events;
+    },
   },
-  methods: {},
+  methods: {
+    handleSelect(arg) {
+      console.log(arg);
+      this.$store.dispatch("addEvent", arg);
+    },
+  },
   components: {
     timeline,
     assignment,
-
     Fullcalendar,
-    student
-  }
+    student,
+  },
 };
 </script>
 
 <style scoped>
-
 .max-height {
-  max-height: 100vh;
+  max-height: 50vh;
 }
 
-.timeline-box {
-  height: 50vh;
+.fc {
+  /* background-color: red; */
 }
-.students-box {
+
+.fc-day {
+  /* background-color: hotpink; */
+}
+
+/* .timeline-box {
+  height: 50vh;
+} */
+/* .students-box {
   height: 37vh;
   overflow-y: auto;
-}
+} */
 </style>
-
