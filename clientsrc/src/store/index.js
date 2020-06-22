310bbd508d7a1ef8224bce02593d3cd6d452098b
chris_MSI1 <chris.h.felt@gmail.com>
2020-06-22 14:09:33 -0600

    socket client side

--- clientsrc/src/store/index.js
+++ clientsrc/src/store/index.js
@@ -7,6 +7,8 @@ import { StudentStore } from "./StudentStore";
 import { AssignmentsStore } from "./AssignmentsStore";
 import { PostsStore } from "./PostsStore";
 import { CommentsStore } from "./CommentsStore";
+import socketStore from "./socketStore";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -64,5 +66,6 @@ export default new Vuex.Store({
     AssignmentsStore,
     PostsStore,
     CommentsStore,
+    socketStore,
   },
 });
