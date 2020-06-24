4f8fdc0950fbd4811211a6f45256cf436e8a55cd
JustinLGates <papagates1@gmail.com>
2020-06-24 11:42:40 -0600

    added search post

--- clientsrc/src/views/Posts.vue
+++ clientsrc/src/views/Posts.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="container-fluid">
     <div class="row push-down-less mb-2">
+      <div class="col-12 col-md-10 col-lg-8 m-auto text-center">
+        <div class="w-100 pt-3">
+          <input
+            v-model="postQuery"
+            class="w-80"
+            type="text"
+            placeholder="becky@BoiseCodeWorks.com"
+          />
+          <button
+            @click="searchPosts,toggleSearch = !toggleSearch"
+            class="btn btn-info"
+          >search posts</button>
+          {{this.postQuery}}
+        </div>
+      </div>
       <div class="col-12">
         <div class="pt-3 text-center w-100">
           <button
@@ -8,26 +23,38 @@
             data-toggle="modal"
             data-target="#addPost"
             class="btn text-dark btn-info"
-          >Share an event or get advice</button>
+          >Share</button>
         </div>
       </div>
     </div>
-    <div class="row justify-content-center post-box">
+    <div v-if="!toggleSearch" class="row justify-content-center post-box">
       <post v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+    <div v-if="toggleSearch" class="row post-box">
+      <post v-for="post in searchPosts" :key="post.id" :post="post" />
     </div>
   </div>
 </template>
 <script>
 import Post from "../components/postsComponents/post";
 export default {
+  data() {
+    return { postQuery: "", toggleSearch: false, postsByquery: [] };
+  },
   mounted() {
     this.$store.dispatch("getAllPosts");
   },
   computed: {
     posts() {
       return this.$store.state.PostsStore.posts;
+    },
+    searchPosts() {
+      let matches = this.posts.filter(p => p.creator.email == this.postQuery);
+      return matches;
     }
   },
+
+  methods: {},
   components: {
     Post
   }
