01c833cdc9b9bbaaa9911a8c013605cca3a05b21
Troy Fuhriman <TkFuhriman@gmail.com>
2020-08-07 18:37:42 -0600

    fixed adding events

--- server/models/Event.js
+++ server/models/Event.js
@@ -4,7 +4,7 @@ let Schema = mongoose.Schema;
 const Event = new Schema(
   {
     creatorEmail: { type: String, required: true },
-    postId: { type: String, required: true },
+    postId: { type: String },
     title: { type: String, required: true },
     body: { type: String, required: true },
     start: { type: Date },
